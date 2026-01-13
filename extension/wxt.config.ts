58f885599792dfa47e9734ed9ac913f027dd2b55
chiwon.song <chiwon.song@voyagerx.com>
2026-01-13 13:43:10 +0900

    Initializes extension with project name and updated icons
    *   Updates project name
    *   Replaces extension icons
    *   Configures manifest name
    *   Adds storage permission

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
+    name: "Who's on meet?",
     permissions: ['storage'],
+    icons: {
+      16: '/icon/16.png',
+      32: '/icon/32.png',
+      48: '/icon/48.png',
+      96: '/icon/96.png',
+      128: '/icon/128.png',
+    },
   },
 });
